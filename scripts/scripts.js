a27fd472e353c06095544efda8f0bc84f42e879d
w Agostina Barbenza <agosbarbenza@gmail.com>
2021-02-16 13:08:50 -0300

    Fixed menu display problem and made some code styling changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,5 @@
 window.onload = ()=>{
     displayMenu();
-    selectHeightTable();
-    selectAverageTable();
-    selectLengthTable();
-    selectElevationTable();
     closeMenu();
 }
 
@@ -11,20 +7,25 @@ function displayMenu(){
     const surfaceBtn = document.getElementById('surface-wrapper');
     const subMenu = document.getElementById('sub-menu');
     surfaceBtn.addEventListener('click', ()=>{
-        if(subMenu.style.display == 'none'){
+        if(subMenu.style.display == 'none' || subMenu.style.display == ''){
             subMenu.style.display = 'block';
         }else{
-            subMenu.style.display = 'none'
+            subMenu.style.display = 'none';
         }   
-    })
+    });
+    selectHeightTable();
+    selectAverageTable();
+    selectLengthTable();
+    selectElevationTable();
 }
 
 function closeMenu(){
     const subMenu = document.getElementById('sub-menu');
     subMenu.addEventListener('mouseleave', ()=>{
-        subMenu.style.display = 'none'
-    })
+        subMenu.style.display = 'none';
+    });
 }
+
 
 function selectHeightTable (){
     const tableFrame = document.getElementById('table-frame');
@@ -32,7 +33,7 @@ function selectHeightTable (){
 
     btn.addEventListener('click', ()=>{
         tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSnxz1u28dW94Mt6dhFRzg2aWUlZ6RYcHeUq2IFu4igu6mQ0RCpTLldqrB6KINYrg/pubhtml?widget=true&amp;headers=false'
-    })
+    });
 }
 
 function selectAverageTable (){
@@ -40,8 +41,8 @@ function selectAverageTable (){
     const btn = document.getElementById('gradient-option');
 
     btn.addEventListener('click', ()=>{
-        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vS7TNX14zSk4_bGCN1gvsbbOZ9wJLvS4JwWMRWC6_ypkOmgfmP_j8U_RLkqCJJRtg/pubhtml?widget=true&amp;headers=false'
-    })
+        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vS7TNX14zSk4_bGCN1gvsbbOZ9wJLvS4JwWMRWC6_ypkOmgfmP_j8U_RLkqCJJRtg/pubhtml?widget=true&amp;headers=false';
+    });
 }
 
 function selectLengthTable (){
@@ -49,8 +50,8 @@ function selectLengthTable (){
     const btn = document.getElementById('length-option');
 
     btn.addEventListener('click', ()=>{
-        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vRiCjCZDW977WCXPDywnhpoKbUqWO7TRqyKY7o9mVA7zeULpUUT8MhbaFCL_c3NZw/pubhtml?widget=true&amp;headers=false'
-    })
+        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vRiCjCZDW977WCXPDywnhpoKbUqWO7TRqyKY7o9mVA7zeULpUUT8MhbaFCL_c3NZw/pubhtml?widget=true&amp;headers=false';
+    });
 }
 
 function selectElevationTable (){
@@ -58,6 +59,6 @@ function selectElevationTable (){
     const btn = document.getElementById('elevation-option');
 
     btn.addEventListener('click', ()=>{
-        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vT9ZbPbKs0rQm9xWpoN67Xek0A_4-WiclgO-Rwlgqts59j6HGEIGDvvBh21PKJQEw/pubhtml?widget=true&amp;headers=false'
-    })
+        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vT9ZbPbKs0rQm9xWpoN67Xek0A_4-WiclgO-Rwlgqts59j6HGEIGDvvBh21PKJQEw/pubhtml?widget=true&amp;headers=false';
+    });
 }
