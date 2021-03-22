d09b85c74f1b85ae1dc581e7d05af3d1b569d256
w Agostina Barbenza <agosbarbenza@gmail.com>
2021-03-21 21:43:11 -0300

    Fixed table select functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,6 +17,7 @@ function displayMenu(){
     selectAverageTable();
     selectLengthTable();
     selectElevationTable();
+    selectSurfaceTable();
 }
 
 function closeMenu(){
@@ -30,36 +31,55 @@ function closeMenu(){
 function selectHeightTable (){
     const tableFrame = document.getElementById('table-frame');
     const btn = document.getElementById('height-option');
+    let option = document.getElementById('sort-option');
 
     btn.addEventListener('click', ()=>{
-        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSnxz1u28dW94Mt6dhFRzg2aWUlZ6RYcHeUq2IFu4igu6mQ0RCpTLldqrB6KINYrg/pubhtml?widget=true&amp;headers=false'
+        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vSnxz1u28dW94Mt6dhFRzg2aWUlZ6RYcHeUq2IFu4igu6mQ0RCpTLldqrB6KINYrg/pubhtml?widget=true&amp;headers=false';
+        option.innerText = 'Height at finish';
+
     });
 }
 
 function selectAverageTable (){
     const tableFrame = document.getElementById('table-frame');
     const btn = document.getElementById('gradient-option');
+    let option = document.getElementById('sort-option');
 
     btn.addEventListener('click', ()=>{
         tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vS7TNX14zSk4_bGCN1gvsbbOZ9wJLvS4JwWMRWC6_ypkOmgfmP_j8U_RLkqCJJRtg/pubhtml?widget=true&amp;headers=false';
+        option.innerText = 'Average gradient';
     });
 }
 
 function selectLengthTable (){
     const tableFrame = document.getElementById('table-frame');
     const btn = document.getElementById('length-option');
+    let option = document.getElementById('sort-option');
 
     btn.addEventListener('click', ()=>{
         tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vRiCjCZDW977WCXPDywnhpoKbUqWO7TRqyKY7o9mVA7zeULpUUT8MhbaFCL_c3NZw/pubhtml?widget=true&amp;headers=false';
+        option.innerText = 'Length';
     });
 }
 
 function selectElevationTable (){
     const tableFrame = document.getElementById('table-frame');
     const btn = document.getElementById('elevation-option');
+    let option = document.getElementById('sort-option');
 
     btn.addEventListener('click', ()=>{
         tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vT9ZbPbKs0rQm9xWpoN67Xek0A_4-WiclgO-Rwlgqts59j6HGEIGDvvBh21PKJQEw/pubhtml?widget=true&amp;headers=false';
+        option.innerText = 'Absolute elevation gain';
     });
 }
 
+function selectSurfaceTable (){
+    const tableFrame = document.getElementById('table-frame');
+    const btn = document.getElementById('surface-option');
+    let option = document.getElementById('sort-option');
+
+    btn.addEventListener('click', ()=>{
+        tableFrame.src='https://docs.google.com/spreadsheets/d/e/2PACX-1vRXjLhxg6ha8bgVEbLPYu1HTG4l2-frdVbUiKZPjxhEWkQ4ruMvuXE9nD12LvvXPA/pubhtml?widget=true&amp;headers=false';
+        option.innerText = 'Surface';
+    });
+}
